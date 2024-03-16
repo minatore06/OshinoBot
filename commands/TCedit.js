a0a78c554cc5ed85345678ce018228926e99c66a
minatore06 <stei2002@hotmail.it>
2024-03-16 15:45:42 +0100

    messsed up timer

--- commands/TCedit.js
+++ commands/TCedit.js
@@ -55,7 +55,7 @@ module.exports = {
                                     if (client.cooldowns.get(this.name).has(interaction.user.id)) {
                                         let expirationTime = client.cooldowns.get(this.name).get(interaction.user.id) + (this.cooldown * 1000);
                                         if (Date.now() < expirationTime) {
-                                            let timeLeft = expirationTime / 1000;
+                                            let timeLeft = expirationTime.toFixed(0);
                                             msgContent = `Puoi cambiare il nome alla stanza solo ogni 10 minuti.\nTempo rimanente: <t:${timeLeft}:R>.`;
                                         }
                                     } else {
